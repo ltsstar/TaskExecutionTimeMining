e2fe0abb1a03f2a577a4986afc659c06e0bd3b75
Michel Kunkler <michel.kunkler@gmail.com>
2024-07-31 18:08:24 +0200

    artificial simulator

---
+++ src/notebooks/artificial_simulator.py
@@ -0,0 +1,257 @@
+from enum import Enum
+import numpy as np
+import random
+
+
+class ActivityTypes(Enum):
+    DIAGNOSIS = 0
+    REPAIR = 1
+    QUALITY_CONTROL = 2
+    FINISHED = 3
+
+
+class ActivityState(Enum):
+    ACTIVATED = 0
+    STARTED = 1
+    COMPLETED = 2
+
+
+class Activity:
+    def __init__(self, id, type, instance):
+        self.id = id
+        self.type = type
+        self.state = ActivityState.ACTIVATED
+        self.instance = instance
+
+    def start(self):
+        self.state = ActivityState.STARTED
+
+    def complete(self):
+        self.state = ActivityState.COMPLETED
+
+    def __str__(self):
+        return self.type.name
+
+
+class Resource:
+    def __init__(self, name):
+        self.name = name
+
+    def sample_duration(self, activity, simulator):
+        raise NotImplementedError
+    
+    def __str__(self):
+        return str(self.name)
+
+
+class DefaultResource(Resource):
+    def sample_duration(self, activity, simulator):
+            return np.where(np.random.choice(a=[0, 1], size=1, p=[0.5, 0.5]),
+                             np.random.normal(200.2, 0.2, 1),
+                             np.random.normal(600, 0.2, 1)
+                           )[0]
+
+
+class ControlFlow:
+    def __init__(self):
+        pass
+
+    def first_activity_type(self):
+        return ActivityTypes.DIAGNOSIS
+
+    def next_activity_type(self, current_activity_type):
+        if current_activity_type == ActivityTypes.DIAGNOSIS:
+            return ActivityTypes.REPAIR
+        elif current_activity_type == ActivityTypes.REPAIR:
+            return ActivityTypes.QUALITY_CONTROL
+        elif current_activity_type == ActivityTypes.QUALITY_CONTROL:
+            return ActivityTypes.FINISHED
+
+
+class ProcessInstance:
+    control_flow = ControlFlow()
+
+    def __init__(self, id):
+        self.id = id
+        self.current_activity_id = 0
+        self.current_activity = None
+        self.activities = []
+        self.finished = False
+
+    def start_instance(self):
+        first_activity_type = self.control_flow.first_activity_type()
+        self.current_activity = Activity(0, first_activity_type, self)
+        self.activities.append(self.current_activity)
+
+    def activate_next_activity(self):
+        if not self.finished:
+            next_activity_type = self.control_flow.next_activity_type(self.current_activity.type)
+            self.current_activity_id += 1
+            self.current_activity = Activity(self.current_activity_id, next_activity_type, self)
+            self.activities.append(self.current_activity)
+        if self.current_activity.type == ActivityTypes.FINISHED:
+            self.finished = True
+
+    def has_finished(self):
+        return self.finished
+    
+    def __str__(self):
+        return str(self.id)
+
+
+class EventType(Enum):
+    INSTANCE_SPAWN = 0
+    ACTIVITY_ACTIVATE = 1
+    ACTIVITY_START = 2
+    ACTIVITY_COMPLETE = 3
+    INSTANCE_COMPLETE = 4
+
+
+class Event:
+    def __init__(self, event_type, event_time, data):
+        self.type = event_type
+        self.time = event_time
+        self.data = data
+
+
+class Resources:
+    def __init__(self, simulator):
+        self.resources = [DefaultResource(1),
+                          DefaultResource(2)]
+        self.idle_resources = self.resources.copy()
+        self.working_resources = []
+        self.simulator = simulator
+
+    def eligible(self, activity, resource):
+        # TODO
+        return True
+
+    def allocate(self, resource):
+        self.working_resources.append(resource)
+        self.idle_resources.remove(resource)
+
+    def free(self, resource):
+        self.idle_resources.append(resource)
+        self.working_resources.remove(resource)
+
+    def sample_duration(self, activity, resource):
+        return resource.sample_duration(activity, self.simulator)
+
+
+class ProcessSimulator:
+    def __init__(self, start_time = 0, logger = None):
+        self.current_time = start_time
+        self.max_process_instance_id = -1
+        self.event_queue = []
+        self.activated_activities = []
+        self.logger = logger
+        self.resources = Resources(self)
+        
+        self.spawn_instance()
+    
+
+    def spawn_instance(self):
+        self.max_process_instance_id += 1
+        next_instance = ProcessInstance(self.max_process_instance_id)
+        next_instance_spawn_time = self.current_time + np.random.exponential(scale = 24)
+        next_instance_spawn_event = Event(EventType.INSTANCE_SPAWN,
+                                          next_instance_spawn_time,
+                                          {'instance' : next_instance})
+        self.event_queue.append(next_instance_spawn_event)
+
+    def activate_activity(self, activity):
+        activate_event = Event(EventType.ACTIVITY_ACTIVATE,
+                                self.current_time,
+                                {'activity' : activity})
+        self.event_queue.append(activate_event)
+
+    def start_activity(self, activity, resource):
+        start_activity = Event(EventType.ACTIVITY_START,
+                               self.current_time,
+                               {'activity' : activity, 'resource' : resource})
+        self.event_queue.append(start_activity)
+
+    def complete_activity(self, activity, resource, activity_completed_time):
+        activity_completed = Event(EventType.ACTIVITY_COMPLETE,
+                                activity_completed_time,
+                                {'activity' : activity, 'resource' : resource})
+        self.event_queue.append(activity_completed)
+
+    def _instance_spawned(self, event):
+        # activate first activity
+        event.data['instance'].start_instance()
+        self.activate_activity(event.data['instance'].current_activity)
+        # set next instance spawn
+        self.spawn_instance()
+
+    def _activity_activated(self, event):
+        self.activated_activities.append(event.data['activity'])
+
+    def _activity_started(self, event):
+        event.data['activity'].start()
+
+    def _activity_completed(self, event):
+        event.data['activity'].complete()
+        self.resources.free(event.data['resource'])
+        event.data['activity'].instance.activate_next_activity()
+        if not event.data['activity'].instance.has_finished():
+            self.activate_activity(event.data['activity'].instance.current_activity)
+
+    def _instance_completed(self, event):
+        pass
+
+    def _start_activated_activities(self):
+        restart = False
+        for activity in self.activated_activities:
+            random.shuffle(self.resources.idle_resources)
+            for resource in self.resources.idle_resources:
+                if self.resources.eligible(activity, resource):
+                    self.resources.allocate(resource)
+                    self.activated_activities.remove(activity)
+                    # set activity start
+                    self.start_activity(activity, resource)
+
+                    # set activity finish
+                    duration = self.resources.sample_duration(activity, resource)
+                    self.complete_activity(activity, resource, self.current_time + duration)
+                    break
+
+
+    def log_event(self, event):
+        if self.logger:
+            self.logger.log(event)
+
+    def simulate(self, max_time = np.inf):
+        self.start_time = self.current_time
+        while len(self.event_queue):
+            if max_time < self.current_time - self.start_time:
+                return
+            current_event = self.event_queue.pop(0)
+            self.current_time = current_event.time
+
+            if current_event.type == EventType.INSTANCE_SPAWN:
+                self._instance_spawned(current_event)
+            elif current_event.type == EventType.ACTIVITY_ACTIVATE:
+                self._activity_activated(current_event)
+            elif current_event.type == EventType.ACTIVITY_START:
+                pass
+            elif current_event.type == EventType.ACTIVITY_COMPLETE:
+                self._activity_completed(current_event)
+            elif current_event.type == EventType.INSTANCE_COMPLETE:
+                self._instance_completed(current_event)
+            
+            self._start_activated_activities()
+            self.event_queue.sort(key = lambda event : event.time)
+            self.log_event(current_event)
+        return
+
+class PrintLogger:
+    def log(self, event):
+        activity = event.data['activity'] if event.type not in [EventType.INSTANCE_COMPLETE, EventType.INSTANCE_SPAWN] else None
+        instance = event.data['instance'] if event.type in [EventType.INSTANCE_COMPLETE, EventType.INSTANCE_SPAWN] else event.data['activity'].instance
+        resource = event.data['resource'] if event.type in [EventType.ACTIVITY_START] else None
+        print(event.time, instance, event.type, activity, resource)
+
+simulator = ProcessSimulator(logger=PrintLogger())
+simulator.simulate(24*7)
+print('hallo')
