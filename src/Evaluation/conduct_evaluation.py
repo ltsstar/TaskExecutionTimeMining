56d01a882f6d89d5f243c0981fcf8117f04a332c
fedora Cloud User <fedora@schlachtschiff.novalocal>
2024-11-05 08:37:56 +0000

    evaluation

--- src/Evaluation/conduct_evaluation.py
+++ src/Evaluation/conduct_evaluation.py
@@ -4,6 +4,13 @@ import matplotlib.dates as md
 import datetime
 import pandas
 from tqdm import tqdm
+from multiprocessing import Pool
+from functools import partial
+import io
+import pickle
+import copy
+import numpy
+from decimal import *
 
 class ConductEvaluation:
     def __init__(self,
@@ -21,7 +28,15 @@ class ConductEvaluation:
         pass
 
     def _get_kde_from_samples(self, samples, ground_truth):
-        kde = gaussian_kde(samples).pdf(ground_truth)[0]
+        #kde = gaussian_kde(samples).pdf(ground_truth)[0]
+        k = lambda x, h : (Decimal(1) / (Decimal(h) * Decimal(numpy.sqrt(2 * numpy.pi)))) * Decimal(numpy.e)**(Decimal(-0.5) * (Decimal(x) / Decimal(h))**Decimal(2))
+        
+        iqr = numpy.percentile(samples, 75) - numpy.percentile(samples, 25)
+        h = 0.9 * min(numpy.std(samples), iqr/1.34) * len(samples)**(-1/5) # silverman rule
+        if iqr == 0 or h== 0:
+            print(samples)
+            print(ground_truth)
+        kde = Decimal(1)/Decimal(len(samples)) * sum([k(ground_truth - sample, h) for sample in samples]) 
         return kde
     
     def _get_real_start_time(self, case_log):
@@ -61,18 +76,42 @@ class ConductEvaluation:
         plt.axvline(x=real_start_time, color='blue')
         plt.show()
 
-    def sample_cases(self, plot_cases=False):
+    def get_case_data(self, case_name):
+        case_log = self.event_log[self.event_log['case:concept:name'] == case_name]
+        real_start_time_ts = self._get_real_start_time(case_log)
+        real_end_time_ts = self._get_real_end_time(case_log)
+        return (case_name, case_log, real_start_time_ts, real_end_time_ts)
+
+    def sample_case(self, case_log):
+        case_samples = [self.sample_model.sample_case(case_log) for i in range(self.n)]
+        return case_samples
+
+    @staticmethod
+    def sample_case_static(case_log, sample_model, n):
+        return [sample_model.sample_case(case_log) for i in range(n)]
+
+    def sample_cases(self, plot_cases=False, multiprocessing=True):
         cases = self.event_log['case:concept:name'].unique()
         results = dict()
-        for case_name in tqdm(cases):
-            case_log = self.event_log[self.event_log['case:concept:name'] == case_name]
-            real_start_time_ts = self._get_real_start_time(case_log)
-            real_end_time_ts = self._get_real_end_time(case_log)
 
-            case_samples = [self.sample_model.sample_case(case_name) for i in range(self.n)]
-            likelihood = self._get_kde_from_samples(case_samples, real_end_time_ts)
+        #prepare case data
+        case_data = dict([(case_name, self.get_case_data(case_name)) for case_name in cases])
 
-            if plot_cases:
-                self._plot_case(case_samples, real_start_time_ts, real_end_time_ts)
-            results[case_name] = likelihood
-        return results
+        if multiprocessing:
+            with Pool(processes=96) as pool:
+                # Use `imap` to track progress with tqdm
+                func = partial(ConductEvaluation.sample_case_static, sample_model=self.sample_model, n=self.n)
+                sample_results = list(tqdm(pool.imap(func, [c[1] for c in case_data.values()]), total=len(cases)))
+        else:
+            sample_results = []
+            func = partial(ConductEvaluation.sample_case_static, sample_model=self.sample_model, n=self.n)
+            for c in tqdm(case_data.values()):
+                r = func(c[1])
+                sample_results.append(r)
+                
+
+        #if plot_cases:
+        #    for r in results:
+        #        self._plot_case(r[0], r[1], r[2])
+        return_results = dict([(c, self._get_kde_from_samples(sample_results[i], case_data[c][3])) for i, c in enumerate(case_data)])
+        return return_results, sample_results, case_data
