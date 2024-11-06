36eed4f682a282299276e662b7639b0d9017b0ab
fedora Cloud User <fedora@kreuzer.novalocal>
2024-11-06 19:19:39 +0000

    eval update

--- src/Evaluation/normal_evaluation/drbart_evaluation.py
+++ src/Evaluation/normal_evaluation/drbart_evaluation.py
@@ -1,3 +1,4 @@
+from functools import partial
 from normal_evaluation.normal_evaluation import *
 
 class SampleOutcomes_DRBART_Normal(SampleOutcomes_Normal):
@@ -6,13 +7,27 @@ class SampleOutcomes_DRBART_Normal(SampleOutcomes_Normal):
         super().__init__(case_event_log, resources, **kwargs)
         self.drbart_model = drbart_model
         self.max_sample = max_sample
+        self.categorical_args = []
+        self.continuous_args = []
 
-
-class SampleOutcomes_DRBART_Normal_A(SampleOutcomes_DRBART_Normal):
     def sample_duration(self, seconds_in_day, resource, concept_name,
                         resource_count, activity_count, day_of_week):
-        sample_time =  lambda : self.drbart_model.sample([concept_name],
-                                            [])[1][0]
+
+        continuous_variables = list()
+        for cv in self.continuous_args:
+            continuous_variables.append(eval(cv))
+        categorical_variables = list()
+        for cv in self.categorical_args:
+            r = eval(cv)
+            if type(r) is list:
+                categorical_variables += r
+            else:
+                categorical_variables.append(r)
+        pf = partial(self.drbart_model.sample,
+                                    categorical_variables,
+                                    continuous_variables
+                    )
+        sample_time = lambda : pf()[1][0]
         for i in range(self.max_sample):
             sampled_time = sample_time()
             if sampled_time > 0:
@@ -22,176 +37,93 @@ class SampleOutcomes_DRBART_Normal_A(SampleOutcomes_DRBART_Normal):
             print(concept_name)
             return 0
         else:
+            print(categorical_variables, continuous_variables, sampled_time)
             return sampled_time
+
+
+class SampleOutcomes_DRBART_Normal_A(SampleOutcomes_DRBART_Normal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name']
+
 
 
 class SampleOutcomes_DRBART_Normal_R(SampleOutcomes_DRBART_Normal):
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([resource],
-                                            [])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(resource)
-            return 0
-        else:
-            return sampled_time
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['resource']
+
 
 
 class SampleOutcomes_DRBART_Normal_R_S(SampleOutcomes_DRBART_Normal):
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([resource],
-                                            [seconds_in_day])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(concept_name, resource)
-            return 0
-        else:
-            return sampled_time
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['resource']
+        self.continuous_args = ['seconds_in_day']
 
 
 class SampleOutcomes_DRBART_Normal_A_R(SampleOutcomes_DRBART_Normal):
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([
-                                                 concept_name,
-                                                 resource
-        ],
-                                            [])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(concept_name, resource)
-            return 0
-        else:
-            return sampled_time
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name', 'resource']
 
 
 class SampleOutcomes_DRBART_Normal_A_R_S(SampleOutcomes_DRBART_Normal):
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([concept_name, resource,
-                                            ],
-                                            [seconds_in_day])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(concept_name, resource, seconds_in_day)
-            return 0
-        else:
-            return sampled_time
-        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name', 'resource']
+        self.continuous_args = ['seconds_in_day']
+
 
 class SampleOutcomes_DRBART_Normal_A_R_S_D(SampleOutcomes_DRBART_Normal):
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([concept_name,
-                                            resource,
-                                            day_of_week
-                                            ],
-                                            [seconds_in_day])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(concept_name, resource, seconds_in_day, day_of_week)
-            return 0
-        else:
-            return sampled_time
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name', 'resource', 'day_of_week']
+        self.continuous_args = ['seconds_in_day']
 
 
 class SampleOutcomes_DRBART_Normal_A_R_S_AC(SampleOutcomes_DRBART_Normal):
-    def __init__(self, case_event_log, drbart_model,
-                 known_activities, **kwargs):
-        super().__init__(case_event_log, drbart_model, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name', 'resource',
+                                  '*[0 if activity not in activity_count else activity_count[activity] for activity in self.known_activities]']
+        self.continuous_args = ['seconds_in_day']
         self.known_activities = known_activities
 
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([
-                                        concept_name,
-                                        resource,
-                                        *[0 if activity not in activity_count else activity_count[activity] for activity in self.known_activities],
-                                        ],
-                                        [seconds_in_day])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(concept_name,  resource, seconds_in_day, activity_count)
-            return 0
-        else:
-            return sampled_time
 
 
 class SampleOutcomes_DRBART_Normal_A_R_S_RC(SampleOutcomes_DRBART_Normal):
-    def __init__(self, case_event_log, drbart_model,
-                 known_resources, **kwargs):
-        super().__init__(case_event_log, drbart_model, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name', 'resource',
+                                  '*[0 if resource not in resource_count else resource_count[resource] for resource in self.known_resources]'
+                                 ]
+        self.continuous_args = ['seconds_in_day']
         self.known_resources = known_resources
 
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([concept_name,
-                                        resource,
-                                        *[0 if resource not in resource_count else resource_count[resource] for resource in self.known_resources]
-                                        ],
-                                        [seconds_in_day])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(concept_name,  resource, seconds_in_day, resource_count)
-            return 0
-        else:
-            return sampled_time
 
 
 class SampleOutcomes_DRBART_Normal_A_R_S_RC_AC(SampleOutcomes_DRBART_Normal):
-    def __init__(self, case_event_log, drbart_model,
-                 known_activities, known_resources, **kwargs):
-        super().__init__(case_event_log, drbart_model, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name', 'resource',
+                                  '*[0 if resource not in resource_count else resource_count[resource] for resource in self.known_resources]'
+                                  '*[0 if activity not in activity_count else activity_count[activity] for activity in self.known_activities]'
+                                 ]
+        self.continuous_args = ['seconds_in_day']
         self.known_activities = known_activities
         self.known_resources = known_resources
 
-    def sample_duration(self, seconds_in_day, resource, concept_name,
-                        resource_count, activity_count, day_of_week):
-        sample_time = lambda : self.drbart_model.sample([
-                                        concept_name,
-                                        resource,
-                                        *[0 if resource not in resource_count else resource_count[resource] for resource in self.known_resources],
-                                        *[0 if activity not in activity_count else activity_count[activity] for activity in self.known_activities],
-                                        ],
-                                        [seconds_in_day])[1][0]
-        for i in range(self.max_sample):
-            sampled_time = sample_time()
-            if sampled_time > 0:
-                break
-        if sampled_time < 0:
-            print('warning sample time below 0:', sampled_time)
-            print(concept_name,  resource, seconds_in_day, activity_count, resource_count)
-            return 0
-        else:
-            return sampled_time
-        
+
+
+class SampleOutcomes_DRBART_Normal_A_R_S_D_RC_AC(SampleOutcomes_DRBART_Normal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.categorical_args =  ['concept_name', 'resource',
+                                  'day_of_week',
+                                  '*[0 if resource not in resource_count else resource_count[resource] for resource in self.known_resources]'
+                                  '*[0 if activity not in activity_count else activity_count[activity] for activity in self.known_activities]'
+                                 ]
+        self.continuous_args = ['seconds_in_day']
+        self.known_activities = known_activities
+        self.known_resources = known_resources   
